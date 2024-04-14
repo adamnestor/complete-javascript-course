d98d1b010c0077841e834c26e3c14b300818259a
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-04-14 11:43:52 -0400

    Update script.js

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -191,7 +191,6 @@ const addVAT2 = addTaxRate(0.23);
 console.log(addVAT2(100));
 console.log(addVAT2(23));
 
-*/
 
 ////////CHALLENGE 1////////////////////////////////
 ////////////////////////////////////////////////////
@@ -200,7 +199,7 @@ const poll = {
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
   // This generates [0, 0, 0, 0]. More in the next section!
   answers: new Array(4).fill(0),
-
+  
   //Task 1
   //Get Answer
   registerNewAnswer() {
@@ -210,16 +209,16 @@ const poll = {
       )
     );
     console.log(answer);
-
+    
     //Register Answer
     typeof answer === 'number' &&
-      answer < this.answers.length &&
-      this.answers[answer]++;
-
+    answer < this.answers.length &&
+    this.answers[answer]++;
+    
     this.displayResults();
     this.displayResults('string');
   },
-
+  
   displayResults(type = 'array') {
     if (type === 'array') {
       console.log(this.answers);
@@ -230,7 +229,96 @@ const poll = {
 };
 
 document
-  .querySelector('.poll')
-  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+.querySelector('.poll')
+.addEventListener('click', poll.registerNewAnswer.bind(poll));
+//////////////////////////////
+const runOnce = function () {
+  console.log('This will never run again');
+};
+
+runOnce;
+
+//Immediate invoked function expression IIFE
+(function () {
+  console.log('This will never run again');
+})();
+
+(() => console.log('This will ALSO never run again'))();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+
+console.log(notPrivate);
+
+////// Closures
+
+const secureBooking = function () {
+  let passengerCount = 0;
+  
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+console.dir(booker);
 
 
+let f;
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+//Example
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+boardPassengers(180, 5);
+*/
+
+//////// CHALLENGE 2
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
+
+//By the time this callback here is executed, this IIFE is now long gone. It has already been excuted. And with it, this variable (header) is basically gone as well. But this function that we wrote is attached to the body element. And so it's waiting for some events to happen there. And when the events happen then this function is of course, executed. And, even thought this environment in which this function was created is already gone, it is still able to access the variables that were created in that variable by the time the function was born, so to say. We can say the header is in the backpack of this function.
