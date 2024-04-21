405468057892210cd0940a29d995fb48b1489225
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-04-21 06:43:02 -0400

    Update script.js

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -295,5 +295,226 @@ tesla.chargeBattery(90);
 console.log(tesla);
 tesla.brake();
 tesla.accelerate();
-*/
 
+// Inheritance Between "Classes: eS6 Classes"
+
+class PersonCl {
+    constructor(fullName, birthYear) {
+        this.fullName = fullName;
+        this.birthYear = birthYear;
+    }
+    
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    }
+    
+    greet() {
+        console.log(`Hey, ${this.fullName}`);
+    }
+    
+    get age() {
+        return 2037 - this.birthYear;
+    }
+    
+    set fullName(name) {
+        console.log(name);
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+}
+
+get fullName() {
+    return this._fullName;
+}
+
+static hey() {
+    console.log(`Hey there 👋`);
+}
+}
+
+class Studentl extends PersonCl {
+    constructor(fullName, birthYear, course) {
+        // ALways needs to happen first! To access this keyword
+        super(fullName, birthYear);
+        this.course = course;
+    }
+    
+    introduce() {
+        console.log(`My name is ${this.fullName} and I study ${this.course}`);
+    }
+    
+    calcAge() {
+        console.log(
+            `I'm ${2037 - this.birthYear} but I feel much older as a student!`
+        );
+    }
+}
+
+const martha = new Studentl('Martha Jones', 2012, 'Computer Science');
+martha.introduce();
+martha.calcAge();
+
+const PersonProto = {
+    calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+    PersonProto.init.call(this, firstName, birthYear);
+    this.course = course;
+};
+
+StudentProto.introduce = function () {
+    console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+
+jay.introduce();
+jay.calcAge();
+
+// 1) Public fields
+// 2) Private fields
+// 3) Public methods
+// 4) Private methods
+
+class Account {
+    //Public fields (instances)
+    locale = navigator.language;
+    
+    //Private fields (instances)
+    #movements = [];
+    #pin;
+    
+    constructor(owner, currency, pin) {
+        this.owner = owner;
+        this.currency = currency;
+        //protected property
+        this.#pin = pin;
+        //this._movements = [];
+        //this.locale = navigator.language;
+        
+        console.log(`Thanks for opening an account, ${owner}`);
+    }
+
+  // 3) Public methods
+  //Public interface
+  getMovements() {
+      return this.#movements;
+    }
+    
+    deposit(val) {
+    this.#movements.push(val);
+    return this;
+}
+
+withdraw(val) {
+    this.deposit(-val);
+    return this;
+}
+
+requestLoan(val) {
+    if (this._approveLoan(val)) {
+        this.deposit(val);
+        console.log(`Loan approved`);
+        return this;
+    }
+}
+
+// 4) Private methods
+//   #approveLoan(val) {
+    _approveLoan(val) {
+        return true;
+    }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111, []);
+
+//acc1.#movements.push(250);
+//acc1.#movements.push(-140);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+//acc1.#approveLoan(1000);
+console.log(acc1.getMovements());
+
+console.log(acc1);
+//console.log(acc1.#pin);
+
+//Chaining
+acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
+console.log(acc1.getMovements());
+*/
+//////////////////////////////////////////
+////CHALLENGE 4/////////////////////////////
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.speed}`);
+    return this;
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.speed}`);
+    return this;
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+class EVCl extends CarCl {
+  #charge;
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+
+  accelerate() {
+    this.speed += 20;
+    this.#charge--;
+    console.log(
+      `${this.make} is going ${this.speed} with a charge of ${this.#charge}`
+    );
+    return this;
+  }
+}
+
+const rivian = new EVCl('Rivian', 120, 23);
+console.log(rivian);
+
+rivian
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .brake()
+  .chargeBattery(50)
+  .accelerate();
+
+console.log(rivian.speedUS);
