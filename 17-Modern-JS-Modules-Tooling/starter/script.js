6a47da4a40a60b469a43667cd4d53cabe5ddd50c
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-04-23 16:27:20 -0400

    update

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,3 +1,45 @@
+/*
 //importing module
-import './shoppingCart';
+// import {
+//   addToCart,
+//   totalPrice as price,
+//   totalQuantity,
+// } from './shoppingCart.js';
+// addToCart('bread', 5);
+// console.log(price, totalQuantity);
+
 console.log('Importing module');
+
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5);
+
+import add, { cart } from './shoppingCart.js';
+add('pizza', 2);
+add('mac and cheese', 4);
+console.log(cart);
+
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+//not very clean
+//lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
+*/
+
+
+
