be570e09432318f97ddc8b80454c661cb7c3a3e5
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-04-23 17:32:37 -0400

    update

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -39,7 +39,60 @@ console.log(lastPost);
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+        `${quantity} ${product} added to cart. Shipping cost is $${shippingCost}`
+    );
+};
+
+const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+};
+
+return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+};
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
+
+// Export (doesnt work for some reason)
+export.addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+        `${quantity} ${product} added to cart. Shipping cost is $${shippingCost}`
+    );
+};
 */
 
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
 
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 2 },
+  ],
+  user: { loggedIn: true },
+};
 
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
